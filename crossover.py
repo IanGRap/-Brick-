72ad93635d1cfd79a75d6bfbe141495de506b9ca
Ian Rapoport <irapopor@ucsc.edu>
2018-12-02 12:07:26 -0800

    intial crossover file with prototype and comments

---
+++ crossover.py
@@ -0,0 +1,8 @@
+
+def crossover(image_one, image_two, fitness_one, fitness_two){
+	"""
+		Input: 2 images with fitness values to crossover
+		Output: A single image made from linearly interpolating the input images
+	"""
+	
+}
