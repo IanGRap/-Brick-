132c81631c8daad0dc1eaea3fdde9d5e30934192
JAlexCarney <JAlexCarney@gmail.com>
2018-12-11 16:10:28 -0800

    minor changes to restock

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -15,55 +15,56 @@ def crossover(individual_one, individual_two):
     return (mutate(Image.blend(image_one, image_two, alpha)), (fitness_one + fitness_two)/2)
 
 def mutate(im):
-	if(random() > 0.2):
-		return im
-	width, height = im.size
-	v = int(random() * 50) + 10
-	vr = int(((random() * 2) - 1) * v)
-	vg = int(((random() * 2) - 1) * v)
-	vb = int(((random() * 2) - 1) * v)
-	colors = {}
-	for x in range(width):
-	    for y in range(height):
-	        r,g,b = im.getpixel((x,y))
-	        if (r, g, b) in colors:
-	        	im.putpixel((x, y), colors[(r, g, b)])
-	        else:
-	        	nr = min(max(0, r + vr), 255)
-	        	ng = min(max(0, g + vg), 255)
-	        	nb = min(max(0, b + vb), 255)
-	        	colors[(r, g, b)] = (nr, ng, nb)
-	        	im.putpixel((x, y), (nr, ng, nb))
-	r = choice(range(3))
-	magnitude = int((0.5 * width) * random()) + int(0.25 * width)
-	if(r is 0):
-		area_one = (0, 0, magnitude, height)
-		area_two = (magnitude + 1, 0, width, height)
-		crop_one = im.crop(area_one)
-		crop_two = im.crop(area_two)
-		w = crop_two.size[0]
-		im.paste(crop_two)
-		im.paste(crop_one, (w, 0))
-	elif(r is 1):
-		area_one = (0, 0, width, magnitude)
-		area_two = (0, magnitude + 1, width, height)
-		crop_one = im.crop(area_one)
-		crop_two = im.crop(area_two)
-		h = crop_two.size[1]
-		im.paste(crop_two)
-		im.paste(crop_one, (0, h))
-	else:
-		mid = (int(random() * width), int(random() * height))
-		crop_tl = im.crop((0, 0, mid[0], mid[1]))
-		crop_bl = im.crop((0, mid[1] + 1, mid[0], height))
-		crop_tr = im.crop((mid[0] + 1, 0, width, mid[1]))
-		crop_br = im.crop((mid[0] + 1, mid[1] + 1, width, height))
-		im.paste(crop_br)
-		im.paste(crop_bl, (crop_br.size[0], 0))
-		im.paste(crop_tr, (0, crop_br.size[1]))
-		im.paste(crop_tl, (crop_br.size[0], crop_br.size[1]))
-	return im.filter(ImageFilter.SHARPEN)
-		
+    if(random() > 0.2):
+        return im
+    print("mutating " + str(im))
+    width, height = im.size
+    v = int(random() * 50) + 10
+    vr = int(((random() * 2) - 1) * v)
+    vg = int(((random() * 2) - 1) * v)
+    vb = int(((random() * 2) - 1) * v)
+    colors = {}
+    for x in range(width):
+        for y in range(height):
+            r,g,b = im.getpixel((x,y))
+            if (r, g, b) in colors:
+                im.putpixel((x, y), colors[(r, g, b)])
+            else:
+                nr = min(max(0, r + vr), 255)
+                ng = min(max(0, g + vg), 255)
+                nb = min(max(0, b + vb), 255)
+                colors[(r, g, b)] = (nr, ng, nb)
+                im.putpixel((x, y), (nr, ng, nb))
+    r = choice(range(3))
+    magnitude = int((0.5 * width) * random()) + int(0.25 * width)
+    if(r is 0):
+        area_one = (0, 0, magnitude, height)
+        area_two = (magnitude + 1, 0, width, height)
+        crop_one = im.crop(area_one)
+        crop_two = im.crop(area_two)
+        w = crop_two.size[0]
+        im.paste(crop_two)
+        im.paste(crop_one, (w, 0))
+    elif(r is 1):
+        area_one = (0, 0, width, magnitude)
+        area_two = (0, magnitude + 1, width, height)
+        crop_one = im.crop(area_one)
+        crop_two = im.crop(area_two)
+        h = crop_two.size[1]
+        im.paste(crop_two)
+        im.paste(crop_one, (0, h))
+    else:
+        mid = (int(random() * width), int(random() * height))
+        crop_tl = im.crop((0, 0, mid[0], mid[1]))
+        crop_bl = im.crop((0, mid[1] + 1, mid[0], height))
+        crop_tr = im.crop((mid[0] + 1, 0, width, mid[1]))
+        crop_br = im.crop((mid[0] + 1, mid[1] + 1, width, height))
+        im.paste(crop_br)
+        im.paste(crop_bl, (crop_br.size[0], 0))
+        im.paste(crop_tr, (0, crop_br.size[1]))
+        im.paste(crop_tl, (crop_br.size[0], crop_br.size[1]))
+    return im.filter(ImageFilter.SHARPEN)
+
 def generate_successors(images):
     """
         Input: List of (image, fitness) after user has evaluated
